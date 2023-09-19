ec408ce00ed0ca5d9bc0cc93af216f99e6abd088
Enes Yumer <enes.12.yumer@gmail.com>
2023-09-19 17:41:53 +0200

    Test2?

--- BINV1010 ALGO/workspace/ALGO_tortue/src/Print.java
+++ BINV1010 ALGO/workspace/ALGO_tortue/src/Print.java
@@ -1,5 +1,6 @@
 public class Print {
     public static void main(String[] args) {
         System.out.println("Bonsoir j'essaie qqch hehe");
+        System.out.println("j'essaie autre chose");
     }
 }
