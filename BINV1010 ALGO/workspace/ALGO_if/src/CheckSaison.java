bf92cb6ff1226fde5e69cfd2d890d8d067e7b685
Enes Yumer <enes.12.yumer@gmail.com>
2023-09-29 00:45:14 +0200

    Add verif

--- BINV1010 ALGO/workspace/ALGO_if/src/CheckSaison.java
+++ BINV1010 ALGO/workspace/ALGO_if/src/CheckSaison.java
@@ -6,23 +6,16 @@ public class CheckSaison {
         int jour = scanner.nextInt();
         System.out.print("Entrez le mois : ");
         int mois = scanner.nextInt();
-        /*if ((mois == 12 && jour >= 21) || (mois >= 1 && mois <= 2) || (mois == 3 && jour <= 20)) {
-            System.out.println("Hiver");
-        } else if (mois == 3 || mois >= 4 && mois <= 5 || mois == 6 && jour <= 20) {
-            System.out.println("print");
-        } else if (mois == 6 || mois >= 7 && mois <= 8 || mois == 9 && jour <= 22) {
-            System.out.println("ete");
-        }// else if (mois == 9 || mois >= 10 && mois <= 11 || mois == 12) {
-            //System.out.println("auto");
-        else System.out.println("auto");
-        } */
 
-
-        String season = detectSeason(mois, jour);
-        System.out.println("The season is: " + season);
+        if (estDateValide(mois, jour)) {
+            String saison = trvSaison(mois, jour);
+            System.out.println("La saison est : " + saison);
+        } else {
+            System.out.println("Date invalide !");
+        }
     }
 
-    public static String detectSeason(int mois, int jour) {
+    public static String trvSaison(int mois, int jour) {
         if ((mois == 12 && jour >= 21) || (mois >= 1 && mois <= 2) || (mois == 3 && jour <= 20)) {
             return "Hiver";
         } else if (mois == 3 || mois >= 4 && mois <= 5 || mois == 6 && jour <= 20) {
@@ -34,5 +27,12 @@ public class CheckSaison {
         } else {
             return "Entrée invalide";
         }
-     }
+    }
+    public static boolean estDateValide(int mois, int jour) {
+        if (mois < 1 || mois > 12) {
+            return false;
+        }
+        int[] joursMax = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return jour >= 1 && jour <= joursMax[mois - 1];
+    }
 }
