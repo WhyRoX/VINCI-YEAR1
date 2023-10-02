33f28084be4e3aa4c156e2afe39457db109d7b33
Enes Yumer <enes.12.yumer@gmail.com>
2023-10-02 13:54:42 +0200

    For

---
+++ BINV1010 ALGO/Docs ALGO/ForSomme5.java
@@ -0,0 +1,11 @@
+public class ForSomme5 {
+
+    public static void main(String[] args) {
+        int somme = 0;
+        for (int i = 1; i <= 3; i++){
+            somme = somme + i ;
+        }
+        System.out.println("La somme est : "+somme);
+    }
+
+}
---
+++ BINV1010 ALGO/workspace/ALGO_tortue/src/TEstssss.java
@@ -0,0 +1,28 @@
+public class TEstssss {
+    public static void main(String[] args) {
+
+        int x = 1;
+
+        if(x<3){
+
+            for (int i = 0; i < 5; i++) {
+
+                x = x+2;
+
+            }
+
+        }else{
+
+            for (int i = 0; i < 3; i++) {
+
+                x = x+1;
+
+            }
+
+        }
+
+        System.out.println(x);
+
+    }
+}
+
