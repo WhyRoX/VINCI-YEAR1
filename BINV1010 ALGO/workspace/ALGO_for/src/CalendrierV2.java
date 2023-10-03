5448a05f3c430601e3601adc0e412754c91d9650
Enes Yumer <enes.12.yumer@gmail.com>
2023-10-03 19:54:43 +0200

    Corr

--- BINV1010 ALGO/workspace/ALGO_for/src/CalendrierV2.java
+++ BINV1010 ALGO/workspace/ALGO_for/src/CalendrierV2.java
@@ -5,19 +5,16 @@ public class CalendrierV2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Demande à l'utilisateur de saisir la longueur du mois
         System.out.print("Entrez la longueur du mois (28, 29, 30 ou 31 jours) : ");
         int longueurMois = scanner.nextInt();
 
-        // Demande à l'utilisateur de saisir le jour de la semaine où le 1er du mois tombe
         System.out.print("Entrez le jour de la semaine où le 1er du mois tombe (1 pour lundi, 2 pour mardi, etc.) : ");
         int premierJour = scanner.nextInt();
 
 
-        // Affichage de l'en-tête du calendrier
+        // Affichage en-tête calendrier
         System.out.println("Lu Ma Me Je Ve Sa Di");
 
-        // Variable pour compter les jours du mois
         int jourActuel = 1;
 
         // Espace initial
@@ -27,7 +24,7 @@ public class CalendrierV2 {
 
         // Boucle pour afficher le calendrier
         while (jourActuel <= longueurMois) {
-            // Affiche le jour actuel
+
             System.out.printf("%2d ", jourActuel);
             jourActuel++;
 
