b773b695a89d014a5cada2b6907b3cd58746df8e
Enes Yumer <enes.12.yumer@gmail.com>
2023-10-09 21:44:27 +0200

    .

--- BINV1020 APOO/workspace/APOO_fiche2/src/OIIO.java
+++ BINV1020 APOO/workspace/APOO_fiche2/src/OIIO.java
@@ -1,5 +1,19 @@
 public class OIIO {
+    public static java.util.Scanner scanner = new java.util.Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("ZDSD");
+
+    char reponse;
+    double totalCotes = 0;
+    do {
+        System.out.println("Entrez une cote (sur 20) : ");
+        double cote = scanner.nextDouble();
+        totalCotes += cote;
+        System.out.println("Encore une cote (Y/N) ? ");
+        reponse = scanner.next().charAt(0);
+        
+    } while (reponse == 'Y');
+
+        System.out.println("La somme des cotes : " + totalCotes);
     }
 }
+
